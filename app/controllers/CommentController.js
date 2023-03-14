7139747ee8b838267f3f625d81e00efd9733aedf
vbetsch <vic.betsch@gmail.com>
2023-03-14 17:08:32 +0100

    :sparkles: CRUD Comments
    [ BONUS ]

--- app/controllers/CommentController.js
+++ app/controllers/CommentController.js
@@ -1,5 +1,20 @@
 const CommentModel = require('../models/CommentModel');
 
+// Get my comments
+async function getMyComments(req, res) {
+    try {
+        const comments = await CommentModel.find({
+            author: req.payload._id
+        });
+
+        return res.status(200).json(comments);
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
 // Add comment
 async function addComment(req, res) {
     try {
@@ -16,7 +31,50 @@ async function addComment(req, res) {
             message: 'Comment added successfully'
         });
     } catch (error) {
-        return res.status(400).json({
+        res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
+// Edit comment
+async function editComment(req, res) {
+    try {
+        const { content } = req.body;
+        const comment = await CommentModel.findById({
+            _id: req.params.id,
+            author: req.payload._id
+        });
+
+        comment.content = content;
+
+        await comment.save();
+
+        return res.status(200).json({
+            message: 'Comment edited successfully'
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
+// Delete comment
+async function deleteComment(req, res) {
+    try {
+        const comment = await CommentModel.findById({
+            _id: req.params.id,
+            author: req.payload._id
+        });
+
+        await comment.deleteOne();
+
+        return res.status(200).json({
+            message: 'Comment deleted successfully'
+        });
+    } catch (error) {
+        res.status(500).json({
             message: error.message
         });
     }
@@ -24,5 +82,8 @@ async function addComment(req, res) {
 
 
 module.exports = {
-    addComment
+    getMyComments,
+    addComment,
+    editComment,
+    deleteComment
 }
--- app/routes/CommentRouter.js
+++ app/routes/CommentRouter.js
@@ -15,4 +15,36 @@ const checkJwt = require('../../middlewares/checkJwt');
  */
 router.post('/comment/:id', checkJwt, CommentController.addComment);
 
+/**
+ * Get my comments
+ * @route GET /comment/me
+ * @summary Response with your comments
+ * @group Comment
+ * @return {String} 200 - Success
+ * @returns {String} 500 - An error message
+ */
+router.get('/comment/me', checkJwt, CommentController.getMyComments);
+
+/**
+ * Edit a comment
+ * @route PUT /comment/edit/{id}
+ * @summary Response with 'Comment edited successfully'
+ * @group Comment
+ * @param {Article.model} object.body.required Object containing properties to edit
+ * @return {String} 200 - Edited with success
+ * @returns {String} 500 - An error message
+ */
+router.put('/comment/edit/:id', checkJwt, CommentController.editComment);
+
+/**
+ * Delete a comment
+ * @route DELETE /comment/delete/{id}
+ * @summary Response with 'Comment deleted successfully'
+ * @group Comment
+ * @param {Article.model} object.body.required Object containing properties to delete
+ * @return {String} 200 - Deleted with success
+ * @returns {String} 500 - An error message
+ */
+router.delete('/comment/delete/:id', checkJwt, CommentController.deleteComment);
+
 module.exports = router;
